529de2021dfc8e7a8c0d0fccd8c1f99e15790a70
unknown <mahith.madwesh@gmail.com>
2021-05-21 22:36:56 +0530

    added React router dom to Menu Items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
 import './App.css';
 
+import { Route, Link } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
-function App() {
+
+const HatsPage = () => {
   return (
     <div>
-      <HomePage />
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
+
+
+
+function App(props) {
+  return (
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+
+
     </div>
   );
 }
